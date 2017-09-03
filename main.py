15c9e8610ee50e6efc9c5076b308bdb161681230
Unknown <fengyuan.geek@gmail.com>
2017-09-03 22:59:43 +0800

    Updated requirements with psycopg2, main updated data model

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
+    name = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, username, email):
-        self.username = username
+    def __init__(self, name, email):
+        self.name = name
         self.email = email 
 
 def __repr__(self):
